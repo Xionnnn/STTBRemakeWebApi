25af359f0fc435d4713ad24d359456476f419d6b
Arvin Hasim <arvin.hasim@binus.ac.id>
2026-03-12 00:35:09 +0700

    #feat get available news endpoint

--- STTB.WebApiStandard.Contracts/RequestModels/News/GetAvailableNewsRequest.cs
+++ STTB.WebApiStandard.Contracts/RequestModels/News/GetAvailableNewsRequest.cs
@@ -1,20 +1,20 @@
 ﻿using MediatR;
-using STTB.WebApiStandard.Contracts.ResponseModels.Events;
+using STTB.WebApiStandard.Contracts.ResponseModels.News;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace STTB.WebApiStandard.Contracts.RequestModels.News
 {
-    public class GetAvailableNewsRequest : IRequest<GetAvailableEventsResponse>
+    public class GetAvailableNewsRequest : IRequest<GetAvailableNewsResponse>
     {
         public string NewsTitle { get; set; } = string.Empty;
         public string CategoryName { get; set; } = string.Empty;
         public string OrderBy { get; set; } = string.Empty;
         public string OrderState { get; set; } = string.Empty;
         public int PageNumber { get; set; } = 1;
-        public int PageSize { get; set; } = 7;
+        public int PageSize { get; set; } = 5;
         public DateTime? EventDate { get; set; }
-        public int? fetchLimit { get; set; } = null;
+        public int? FetchLimit { get; set; } = null;
     }
 }
