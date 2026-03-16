b9b206daf0d44be664df49ed16e6ae32618c32d9
Arvin Hasim <arvin.hasim@binus.ac.id>
2026-03-16 08:46:21 +0700

    #feat donor member entities

---
+++ STTB.WebApiStandard.Entities/DonorMember.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace STTB.WebApiStandard.Entities;
+
+public partial class DonorMember
+{
+    public long Id { get; set; }
+
+    public string FirstName { get; set; } = null!;
+
+    public string? LastName { get; set; }
+
+    public string? Salutation { get; set; }
+
+    public string? Contact { get; set; }
+
+    public string? Address { get; set; }
+
+    public string? Email { get; set; }
+
+    public string DonationType { get; set; } = null!;
+
+    public bool ProofOfSupport { get; set; }
+
+    public string? ProofOfDonationPath { get; set; }
+
+    public decimal DonationAmount { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public virtual DonorScholarshipDetail? DonorScholarshipDetail { get; set; }
+}
---
+++ STTB.WebApiStandard.Entities/DonorScholarshipDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace STTB.WebApiStandard.Entities;
+
+public partial class DonorScholarshipDetail
+{
+    public long Id { get; set; }
+
+    public long DonorMemberId { get; set; }
+
+    public string StudentName { get; set; } = null!;
+
+    public long AcademicProgramId { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public DateTime UpdatedAt { get; set; }
+
+    public virtual AcademicProgram AcademicProgram { get; set; } = null!;
+
+    public virtual DonorMember DonorMember { get; set; } = null!;
+}
