7c5acd72a011f3829eb70ed8fc044fcbed76c35b
Arvin Hasim <arvin.hasim@binus.ac.id>
2026-04-07 14:40:59 +0700

    #feat adding existing data validation for news and events

--- STTB.WebApiStandard/Validators/CMS/Events/AddEventValidator.cs
+++ STTB.WebApiStandard/Validators/CMS/Events/AddEventValidator.cs
@@ -1,15 +1,32 @@
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 using STTB.WebApiStandard.Contracts.RequestModels.CMS.Events;
+using STTB.WebApiStandard.Contracts.RequestModels.CMS.News;
+using STTB.WebApiStandard.Entities;
 
 namespace STTB.WebApiStandard.Validators.CMS.Events
 {
     public class AddEventValidator : AbstractValidator<AddEventRequest>
     {
-        public AddEventValidator()
+        private readonly SttbDbContext _db;
+        public AddEventValidator(SttbDbContext db)
         {
+            _db = db;
+
             RuleFor(x => x.EventTitle).NotEmpty().WithMessage("Event Title is required.");
             RuleFor(x => x.StartsAtDate).NotEmpty().WithMessage("Start Date is required.");
             RuleFor(x => x.Slug).NotEmpty().WithMessage("Slug is required.");
+            RuleFor(x => x).CustomAsync(ValidateBusinessAsync);
+        }
+        private async Task ValidateBusinessAsync(AddEventRequest request, ValidationContext<AddEventRequest> context, CancellationToken ct)
+        {
+            var existingEvents= await _db.Events
+                .FirstOrDefaultAsync(e => e.Slug == request.Slug || e.Title.ToUpper() == request.EventTitle.ToUpper());
+
+            if (existingEvents != null)
+            {
+                context.AddFailure(nameof(AddEventRequest), "Data Already Exist");
+            }
         }
     }
 }
--- STTB.WebApiStandard/Validators/CMS/News/AddNewsValidator.cs
+++ STTB.WebApiStandard/Validators/CMS/News/AddNewsValidator.cs
@@ -1,14 +1,30 @@
 using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 using STTB.WebApiStandard.Contracts.RequestModels.CMS.News;
+using STTB.WebApiStandard.Contracts.RequestModels.CMS.Users.Permissions;
+using STTB.WebApiStandard.Entities;
 
 namespace STTB.WebApiStandard.Validators.CMS.News
 {
     public class AddNewsValidator : AbstractValidator<AddNewsRequest>
     {
-        public AddNewsValidator()
+        private readonly SttbDbContext _db;
+        public AddNewsValidator(SttbDbContext db)
         {
+            _db = db;
             RuleFor(x => x.Title).NotEmpty().WithMessage("News Title is required.");
             RuleFor(x => x.Slug).NotEmpty().WithMessage("Slug is required.");
+            RuleFor(x => x).CustomAsync(ValidateBusinessAsync);
+        }
+        private async Task ValidateBusinessAsync(AddNewsRequest request, ValidationContext<AddNewsRequest> context, CancellationToken ct)
+        {
+            var existingNews = await _db.NewsPosts
+                .FirstOrDefaultAsync(np => np.Slug == request.Slug || np.Title.ToUpper() == request.Title.ToUpper());
+
+            if (existingNews != null)
+            {
+                context.AddFailure(nameof(AddNewsRequest), "Data Already Exist");
+            }
         }
     }
 }
