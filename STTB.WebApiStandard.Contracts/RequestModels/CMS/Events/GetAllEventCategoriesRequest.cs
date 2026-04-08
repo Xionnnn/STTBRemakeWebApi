9f3c4a58d8def0c20fd24b80551c4ce21ee7ec43
Arvin Hasim <arvin.hasim@binus.ac.id>
2026-04-08 15:41:41 +0700

    #feat delete old get all event categories page

--- STTB.WebApiStandard.Contracts/RequestModels/CMS/Events/GetAllEventCategoriesRequest.cs
+++
@@ -1,9 +0,0 @@
-using MediatR;
-using STTB.WebApiStandard.Contracts.ResponseModels.CMS.Events;
-
-namespace STTB.WebApiStandard.Contracts.RequestModels.CMS.Events
-{
-    public class GetAllEventCategoriesRequest : IRequest<GetAllEventCategoriesResponse>
-    {
-    }
-}
--- STTB.WebApiStandard.Contracts/ResponseModels/CMS/Events/Categories/GetAllEventCategoriesResponse.cs
+++ STTB.WebApiStandard.Contracts/ResponseModels/CMS/Events/Categories/GetAllEventCategoriesResponse.cs
@@ -4,9 +4,14 @@ namespace STTB.WebApiStandard.Contracts.ResponseModels.CMS.Events.Categories
 {
     public class GetAllEventCategoriesResponse
     {
-        public List<GetAllEventCategoriesDTO> Items { get; set; } = new();
+        public List<GetAllEventCategoriesDTO> Items { get; set; } = new List<GetAllEventCategoriesDTO>();
+
+        //pagination meta data
         public int PageNumber { get; set; }
         public int PageSize { get; set; }
+        public int TotalNews { get; set; }
         public int TotalPages { get; set; }
+        public bool HasPreviousPage => PageNumber > 1;
+        public bool HasNextPage => PageNumber < TotalPages;
     }
 }
