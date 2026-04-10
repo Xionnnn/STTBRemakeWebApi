c8ff09155b1407d4a4950f8a2d7593b1af952b18
Arvin Hasim <arvin.hasim@binus.ac.id>
2026-04-10 16:24:20 +0700

    #fix inconsistent library storing path

--- STTB.WebApiStandard/RequestHandlers/Web/Libraries/AddLibraryMemberHandler.cs
+++ STTB.WebApiStandard/RequestHandlers/Web/Libraries/AddLibraryMemberHandler.cs
@@ -1,5 +1,4 @@
 using MediatR;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using STTB.WebApiStandard.Contracts.RequestModels.Web.Libraries;
 using STTB.WebApiStandard.Contracts.ResponseModels.Libraries;
@@ -10,12 +9,10 @@ namespace STTB.WebApiStandard.RequestHandlers.Web.Libraries
     public class AddLibraryMemberHandler : IRequestHandler<AddLibraryMemberRequest, AddLibraryMemberResponse>
     {
         private readonly SttbDbContext _db;
-        private readonly IWebHostEnvironment _env;
 
-        public AddLibraryMemberHandler(SttbDbContext db, IWebHostEnvironment env)
+        public AddLibraryMemberHandler(SttbDbContext db)
         {
             _db = db;
-            _env = env;
         }
 
         public async Task<AddLibraryMemberResponse> Handle(AddLibraryMemberRequest request, CancellationToken ct)
@@ -35,9 +32,9 @@ namespace STTB.WebApiStandard.RequestHandlers.Web.Libraries
             await _db.LibraryMembers.AddAsync(libraryMember, ct);
             await _db.SaveChangesAsync(ct);
 
-            await SaveImageAssetAsync(request.PassportImage!, libraryMember.Id, "passport_image", @"library_members\passport_image", ct);
-            await SaveImageAssetAsync(request.IdImage!, libraryMember.Id, "id_image", @"library_members\id_image", ct);
-            await SaveImageAssetAsync(request.ProofOfDepositImage!, libraryMember.Id, "proof_of_deposit_image", @"library_members\proof_of_deposit_image", ct);
+            await SaveImageAssetAsync(request.PassportImage!, libraryMember.Id, @"library_members\passport_image", ct);
+            await SaveImageAssetAsync(request.IdImage!, libraryMember.Id, @"library_members\id_image", ct);
+            await SaveImageAssetAsync(request.ProofOfDepositImage!, libraryMember.Id, @"library_members\proof_of_deposit_image", ct);
 
             return new AddLibraryMemberResponse
             {
@@ -47,14 +44,14 @@ namespace STTB.WebApiStandard.RequestHandlers.Web.Libraries
             };
         }
 
-        private async Task SaveImageAssetAsync(IFormFile file, long memberId, string columnName, string modelType, CancellationToken ct)
+        private async Task SaveImageAssetAsync(IFormFile file, long memberId, string modelType, CancellationToken ct)
         {
             var extension = Path.GetExtension(file.FileName);
             var uuid = Guid.NewGuid().ToString();
             var fileName = $"{uuid}{extension}";
-            var relativePath = $"uploads/images/{columnName}/{fileName}";
+            var relativePath = $"/Uploads/images/library_members/{fileName}";
 
-            var physicalDir = Path.Combine(_env.WebRootPath, "uploads", "images", columnName);
+            var physicalDir = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Uploads", "images", "library_members");
             Directory.CreateDirectory(physicalDir);
 
             var physicalPath = Path.Combine(physicalDir, fileName);
