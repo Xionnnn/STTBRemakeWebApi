749c27dd95264fd421302c9dd97ac52c4126c4db
Arvin Hasim <arvin.hasim@binus.ac.id>
2026-04-16 20:38:09 +0700

    #fix prod settings

--- STTB.WebApiStandard.Entities/SttbDbContext.cs
+++ STTB.WebApiStandard.Entities/SttbDbContext.cs
@@ -102,8 +102,12 @@ public partial class SttbDbContext : DbContext
     public virtual DbSet<UserRole> UserRoles { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=sttb_db;Username=postgres;Password=postgres312005");
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Port=5433;Database=sttb_db;Username=postgres;Password=postgres312005");
+        }
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
